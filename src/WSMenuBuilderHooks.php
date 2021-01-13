1b06751324572ab1bb47ba3977f691d3c06288b3
robis <robis@wikibase.nl>
2021-01-13 15:13:02 +0100

    removed hook

--- src/WSMenuBuilderHooks.php
+++ src/WSMenuBuilderHooks.php
@@ -10,17 +10,13 @@ class WSMenuBuilderHooks {
    // Render the output of {{#example:}}.
    public static function buildMenu( Parser $parser, $param1 = '', $param2 = '', $param3 = '' ) {
 
-
       // The input parameters are wikitext with templates expanded.
       // The output should be wikitext too.
       $output = "<div id='app' ></div><div id='vuedata' data-menus='$param1'></div>";
 
-
+      $parser->getOutput()->addModules( 'ext.WSMenuBuilder.module' );
 
        return [ $output, 'noparse' => true, 'isHTML' => true ];
    }
 
-public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
-    $out->addModules( 'ext.WSMenuBuilder.module' );
-   }
 }
