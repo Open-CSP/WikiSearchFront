dada267fa8255f339c0d8cbd0cf2cba9646dd331
robis <robis@wikibase.nl>
2021-03-12 15:31:35 +0100

    range filter last tweak

--- module/wssearch.umd.js
+++ module/wssearch.umd.js
@@ -7399,7 +7399,7 @@ var store_updateStore = function updateStore(store) {
 
             var pessa = [];
 
-            for (var i = 1; i < max; i += step) {
+            for (var i = 1; i < max + 1; i += step) {
               // pessa.push( { "from": i, "to": i + step, "key": (i + step) + '' } )
               pessa.push({
                 "from": i,
