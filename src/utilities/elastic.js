eb9fecdce8e84bd95a7573b59d113737dee1700f
robis <robis@wikibase.nl>
2022-09-20 13:05:57 +0200

    fix empty string

--- src/utilities/elastic.js
+++ src/utilities/elastic.js
@@ -1,7 +1,7 @@
-const insertWildcards = (term) => `*${term.split(' ')
+const insertWildcards = (term) => (term ? `*${term.split(' ')
   .filter(e => e)
   .reduce((a, b) => (/[^a-z_\-0-9]/i.test(a.slice(-1)) ? `${a} ${b}` : `${a}*${b}`))
-  .replace(/\*+/g, '*')}*`;
+  .replace(/\*+/g, '*')}*` : '*');
 
 const prepareQuery = (term) => {
   let searchTerm = term.trim();
