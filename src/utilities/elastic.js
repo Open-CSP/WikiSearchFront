8bf5b3386cd51c9d5927cf7a68b6d7793cdcc515
robis <robis@wikibase.nl>
2022-09-21 09:15:16 +0200

    space between words

--- src/utilities/elastic.js
+++ src/utilities/elastic.js
@@ -1,6 +1,6 @@
 const insertWildcards = (term) => (term ? `*${term.split(' ')
   .filter(e => e)
-  .reduce((a, b) => (/[^a-z_\-0-9]/i.test(a.slice(-1)) ? `${a} ${b}` : `${a}*${b}`))
+  .reduce((a, b) => (/[^a-z_\-0-9]/i.test(a.slice(-1)) ? `${a} ${b}` : `${a}* *${b}`))
   .replace(/\*+/g, '*')}*` : '*');
 
 const prepareQuery = (term) => {
