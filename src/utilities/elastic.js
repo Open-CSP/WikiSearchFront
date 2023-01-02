39cd87bbdf8e35d563c860f3b94e4a8492aa8c9b
Bertrand Gorge <bertrand.gorge@neayi.com>
2023-01-02 18:23:17 +0100

    Fixed #6 - add optional fuzziness

--- src/utilities/elastic.js
+++ src/utilities/elastic.js
@@ -1,7 +1,15 @@
-const insertWildcards = (term) => (term ? `*${term.split(' ')
-  .filter(e => e)
-  .reduce((a, b) => (/[^a-z_\-0-9]/i.test(a.slice(-1)) ? `${a} ${b}` : `${a}* *${b}`))
-  .replace(/\*+/g, '*')}*` : '*');
+const insertWildcards = (term) => {
+  // Add fuzziness
+  // eslint-disable-next-line no-undef
+  if (mw.config.values.WikiSearchFront.config.settings['fuzzy search'] === 'true') {
+    return term.split(' ').join('~ ').trim().concat('~');
+  }
+
+  return (term ? `*${term.split(' ')
+    .filter(e => e)
+    .reduce((a, b) => (/[^a-z_\-0-9]/i.test(a.slice(-1)) ? `${a} ${b}` : `${a}* *${b}`))
+    .replace(/\*+/g, '*')}*` : '*');
+};
 
 const prepareQuery = (term) => {
   let searchTerm = term.trim();
