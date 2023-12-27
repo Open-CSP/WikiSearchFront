72584c7dadada51e231d3ee4b94595d6a6ab3809
Marijn van Wezel <96489967+marijnvanwezel@users.noreply.github.com>
2023-12-27 17:19:32 +0100

    Remove unused function

--- src/utilities/elastic.js
+++ src/utilities/elastic.js
@@ -1,8 +1,3 @@
-const insertWildcards = (term) => (term ? `*${term.split(' ')
-  .filter(e => e)
-  .reduce((a, b) => (/[^a-z_\-0-9]/i.test(a.slice(-1)) ? `${a} ${b}` : `${a}* *${b}`))
-  .replace(/\*+/g, '*')}*` : '*');
-
 const prepareQuery = (term) => {
   return term.trim();
 };
