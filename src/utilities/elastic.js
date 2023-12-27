d430bde7d874672bc63d57bf2479b70a57cbcb6e
Marijn van Wezel <96489967+marijnvanwezel@users.noreply.github.com>
2023-12-27 17:18:18 +0100

    Remove query preparation from frontend

--- src/utilities/elastic.js
+++ src/utilities/elastic.js
@@ -4,17 +4,7 @@ const insertWildcards = (term) => (term ? `*${term.split(' ')
   .replace(/\*+/g, '*')}*` : '*');
 
 const prepareQuery = (term) => {
-  let searchTerm = term.trim();
-  if (searchTerm.length === 0) {
-    return '*';
-  }
-  searchTerm = searchTerm
-    .replace(/(:|\+|=|\/)/g, '\\$1');
-
-  return ['"', "'", 'AND', 'NOT', 'OR', '~', '(', ')', '?', '*', ' -']
-    .reduce((a, b) => a || searchTerm.indexOf(b) !== -1, false)
-    ? searchTerm
-    : insertWildcards(searchTerm);
+  return term.trim();
 };
 
 export default prepareQuery;
